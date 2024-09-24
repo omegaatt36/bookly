73e2052a8bd383e632e5b505427306318d1b5127
omegaatt36 <omegaatt36@gmail.com>
2024-09-24 22:49:50 +0800

    fix: remove prefix 'api' from routers of web.

--- app/web/router.go
+++ app/web/router.go
@@ -11,14 +11,14 @@ func (s *Server) registerRoutes() {
 	router.HandleFunc("GET /accounts/{account_id}/ledgers/new", authenticatedHandler(s.pageCreateLedger))
 
 	// api
-	router.HandleFunc("POST /api/login", s.login)
-	router.HandleFunc("POST /api/logout", s.logout)
-	router.HandleFunc("GET /api/accounts", authenticatedHandler(s.accountList))
-	router.HandleFunc("GET /api/accounts/{account_id}", authenticatedHandler(s.getAccount))
-	router.HandleFunc("GET /api/accounts/{account_id}/ledgers", authenticatedHandler(s.getLedgersByAccount))
+	router.HandleFunc("POST /login", s.login)
+	router.HandleFunc("POST /logout", s.logout)
+	router.HandleFunc("GET /accounts", authenticatedHandler(s.accountList))
+	router.HandleFunc("GET /accounts/{account_id}", authenticatedHandler(s.getAccount))
+	router.HandleFunc("GET /accounts/{account_id}/ledgers", authenticatedHandler(s.getLedgersByAccount))
 
-	router.HandleFunc("POST /api/accounts", authenticatedHandler(s.createAccount))
-	router.HandleFunc("POST /api/accounts/{account_id}/ledgers", authenticatedHandler(s.createLedger))
+	router.HandleFunc("POST /accounts", authenticatedHandler(s.createAccount))
+	router.HandleFunc("POST /accounts/{account_id}/ledgers", authenticatedHandler(s.createLedger))
 
 	s.router = logging(router)
 }
